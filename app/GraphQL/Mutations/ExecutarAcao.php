db0ebb5686acd1060fd2f6e81fa5f6d77ff0838a
fabio-ferraz-sh3 <fabio@sh3.com.br>
2025-07-04 09:33:23 -0400

    degub

--- app/GraphQL/Mutations/ExecutarAcao.php
+++ app/GraphQL/Mutations/ExecutarAcao.php
@@ -14,9 +14,9 @@ final readonly class ExecutarAcao
     {
         $tipo = $args['tipo'];
 
-        Log::debug('Valor da variável:', ['tipo' => $tipo]);
+       // Log::debug('Valor da variável:', ['tipo' => $tipo]);
 
-        if ($tipo === 'monitor') {
+        if ($tipo == 'monitor') {
 
             $data      = \json_decode($args['data'],true);
 
@@ -30,12 +30,16 @@ final readonly class ExecutarAcao
 
             $artisanPath = base_path();
 
+             $texto = 'executando no hohup '. PHP_EOL; //quebra de linha
+
+            \file_put_contents($artisanPath.'/public/Saida.txt',$texto,FILE_APPEND);
+
             $Path = $artisanPath. '/app/Service';
             
             //$C = "nohup php /var/www/html/TimerTrigger.php $INT_FASE_ITEM $CNPJ_PREF $INT_CTA $INT_USU $LG_EMPATE $LG_REIN_ITEM  > /dev/null & echo \$!";	
             $C = "nohup php  $Path/TimerTrigger.php $INT_FASE_ITEM $CNPJ_PREF $INT_CTA $INT_USU $LG_EMPATE $LG_REIN_ITEM $artisanPath > /dev/null & echo \$!";
             
-            Log::debug('Valor da variável:', ['nohup' => $C]);
+          //  Log::debug('Valor da variável:', ['nohup' => $C]);
             $teste = shell_exec($C);		
 
             //MonitorJob::dispatch($args);
